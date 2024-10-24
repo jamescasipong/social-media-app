5fadc6d9fa8d419ba61773fd3c841bf02f312d19
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-10-24 17:17:46 +0800

    xd

---
+++ next-sitemap-config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://socmediahub.vercel.app/",
+  generateRobotsTxt: true,
+  sitemapSize: 5000, // Adjust this value if needed
+};
